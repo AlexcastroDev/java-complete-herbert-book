0e4de294664c340dbe94053b27ac32d10ecc6dae
Alex Oliveira <alexoliveira7x@gmail.com>
2024-04-28 19:24:51 +0100

    chore: dumb example

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Declare a variable called num
+        int num;
+
+        // Assign a value to num
+        num = 100;
+        System.out.println("This is num: " + num);
+
+        // I can reassign the value of num
+        num = num * 2;
+
+        // Print the value of num
+        // Here the num variable is being concatenated with the string
+        // but only the value of num will be printed
+        System.out.print("The value of num * 2 is " + num);
     }
 }
